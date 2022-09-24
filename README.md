4b8b3fdda0fb3e05244329505d8a87b6f55053cf
lee-treehouse <leentaylor@gmail.com>
2022-09-24 13:22:14 +1000

    add db creation docker command to readme, connect to db

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hello-world-sequelize
+
 digital ocean sequelize tutorial from here https://www.digitalocean.com/community/tutorials/how-to-use-sequelize-with-node-js-and-mysql
+
+## create mySql DB running in docker container
+
+`docker run -p 13306:3306 --name hello-world-sequelize -e MYSQL_ROOT_PASSWORD=newPass -e MYSQL_DATABASE=HWS-db -e MYSQL_USER=hws-dev -e MYSQL_PASSWORD=hws-pass -d mysql:latest`
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const { Sequelize } = require('sequelize')
-const sequelize = require('sequelize')
-const sequelize = new Sequelize('hello_world_db', 'lee', 'leePass', {
-    host: 'http://localhost',
-    dialect: 'mysql',
+require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+        charset: 'utf8',
+    },
+    logging: true,
 })
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.')
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database: ', error)
+    })
