07db69cc48315304808e64e843bcf5186423762a
lee-treehouse <leentaylor@gmail.com>
2022-09-25 14:56:50 +1000

    insert book

--- book.controller.js
+++ book.controller.js
@@ -39,6 +39,18 @@ sequelize
     .sync()
     .then(() => {
         console.log('Book table created successfully!');
+        Book.create({
+            title: 'Clean Code',
+            author: 'Robert Cecil Martin',
+            release_date: '2021-12-14',
+            subject: 3,
+        })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.error('Failed to create a new record : ', error);
+            });
     })
     .catch((error) => {
         console.error('Unable to create table : ', error);
