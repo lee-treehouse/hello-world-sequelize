86ee84569cbdf9144086bbf33bc7eeff631b9cec
lee-treehouse <leentaylor@gmail.com>
2022-09-25 15:00:20 +1000

    find all books, move create book logic to separate method

--- book.controller.js
+++ book.controller.js
@@ -38,20 +38,29 @@ const Book = sequelize.define('books', {
 sequelize
     .sync()
     .then(() => {
-        console.log('Book table created successfully!');
-        Book.create({
-            title: 'Clean Code',
-            author: 'Robert Cecil Martin',
-            release_date: '2021-12-14',
-            subject: 3,
-        })
+        Book.findAll()
             .then((res) => {
                 console.log(res);
             })
             .catch((error) => {
-                console.error('Failed to create a new record : ', error);
+                console.error('Failed to retrieve data : ', error);
             });
     })
     .catch((error) => {
         console.error('Unable to create table : ', error);
     });
+
+const createBook = () => {
+    Book.create({
+        title: 'Clean Code',
+        author: 'Robert Cecil Martin',
+        release_date: '2021-12-14',
+        subject: 3,
+    })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((error) => {
+            console.error('Failed to create a new record : ', error);
+        });
+};
